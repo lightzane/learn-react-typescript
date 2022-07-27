99cf6ccec86a12adbe73c34f3bbdc50478971ab4
lightzane <bhest.pat@gmail.com>
2022-07-27 20:38:17 +0800

    update from BrowserRouter to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.scss';
 import { App } from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { FavoritesProvider } from './store/favorites.context';
 
 const root = ReactDOM.createRoot(
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <FavoritesProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </FavoritesProvider>
   </React.StrictMode>
 );
